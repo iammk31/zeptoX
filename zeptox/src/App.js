a451cc3f473774735a4db8a639f7e87df31834e1
Mohit Kumar <80189839+iammk31@users.noreply.github.com>
2023-11-28 14:56:48 +0530

    app js modified

--- zeptox/src/App.js
+++ zeptox/src/App.js
@@ -3,8 +3,8 @@ import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './Components/Auth/SignUp';
 import LogIn from './Components/Auth/LogIn';
-import Content from './Components/Content';
-import Footer from './Components/Footer';
+// import Content from './Components/Content';
+// import Footer from './Components/Footer';
 function App() {
   return (
     <Router>
@@ -14,8 +14,8 @@ function App() {
         <Route path="/LogIn" element={<LogIn />} />
 
       </Routes>
-      <Content />
-      <Footer />
+      {/* <Content /> */}
+      {/* <Footer /> */}
     </Router>
   );
 }
